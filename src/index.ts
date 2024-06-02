cbc34cef65c84a39321d764c0a81099abdc53fc4
Степан Рындин <step.rindin@yandex.ru>
2024-06-02 12:39:16 +0300

    Реализовал создания To-do

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,51 @@
-import { v4 as uuidv4 } from 'uuid';
+type Todo = {
+  name: string;
+  сompleted: boolean;
+  date: Date;
+};
 
+const submitButtonElement =
+  document.querySelector<HTMLButtonElement>('.button');
+const inputElement = document.querySelector<HTMLInputElement>('.input');
+const formElement = document.querySelector<HTMLFormElement>('.form');
+const todoListElement = document.querySelector<HTMLUListElement>('.task-list');
+const templateElement = document
+  .querySelector<HTMLTemplateElement>('#template')
+  ?.content.querySelector<HTMLLIElement>('.task');
+console.log(templateElement);
 
-console.log('123132');
+function handleSubmit(e: Event): void {
+  e.preventDefault();
+
+  if (inputElement && todoListElement) {
+    const data: Todo = {
+      name: inputElement?.value,
+      сompleted: false,
+      date: new Date()
+    };
+
+    renderTodo(data, todoListElement)
+  }
+
+  formElement?.reset()
+}
+
+function createTodo(data: Todo) {
+  const todoEleemnt: HTMLLIElement | null = templateElement?.cloneNode(
+    true,
+  ) as HTMLLIElement;
+  const todoNameElement =
+    todoEleemnt.querySelector<HTMLParagraphElement>('.task__name');
+
+  if (todoNameElement) {
+    todoNameElement.textContent = data.name;
+  }
+
+  return todoEleemnt;
+}
+
+function renderTodo(data: Todo, container: HTMLUListElement) {
+  container.append(createTodo(data));
+}
+
+formElement?.addEventListener('submit', handleSubmit);
